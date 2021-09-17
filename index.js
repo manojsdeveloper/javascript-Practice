40a70e0b061c67167271f3671c2c62af2b8cd8cd
manojsdeveloper <manoj.antony32@gmail.com>
2021-09-17 18:05:58 +0530

    /javascript-truthy-falsy

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
+//Day-01
+if (value) {
+  // value is truthy
+} else {
+  // value is falsy
+  // it could be false, 0, '', null, undefined or NaN
+}
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
